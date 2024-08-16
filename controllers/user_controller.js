7ed501fd661b6ead48516a4864f2744ca4049936
Ellen Ntiamoah <ellenntiamoah@gmail.com>
2024-08-16 17:57:31 +0000

    updates

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,7 +5,7 @@ import { userSchema } from "../schema/user_schema.js";
 
 
 export const signup = async (req, res) => {
-    const {error, value} = userSchema.validate(req.body)
+    const {error, value} = userSchema.validate({...req.body})
     if(error){
         return res.status(400).send(error.details[0].message)
     }
